074607cd258929a5d90f8de71d2ae9d479fb9cde
Marti <martineausimon@hotmail.com>
2024-03-29 01:09:33 -0400

    migrate done

---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,32 @@
+Create table band(
+
+    bandName varchar(50),
+    creation YEAR,
+    genre varchar(50),
+
+    PRIMARY KEY(bandName)
+);
+
+INSERT INTO band values("Crazy Duo", 2015, "rock");
+INSERT INTO band values("Luna", 2009, "classical");
+INSERT INTO band values("Mysterio", 2019, "pop");
+
+ALTER TABLE singer RENAME TO musician;
+
+ALTER TABLE musician
+ADD COLUMN role varchar(50);
+ALTER TABLE musician
+ADD COLUMN bandName varchar(50);
+
+ALTER TABLE musician change singerName musicianName varchar(50);
+
+UPDATE musician SET role = "vocals" WHERE musicianName = "Alina";
+UPDATE musician SET role = "guitar" WHERE musicianName = "Mysterio";
+UPDATE musician SET role = "percussion" WHERE musicianName = "Rainbow";
+UPDATE musician SET role = "piano" WHERE musicianName = "Luna";
+
+UPDATE musician SET bandName = "Crazy Duo" WHERE musicianName = "Alina";
+UPDATE musician SET bandName = "Mysterio" WHERE musicianName = "Mysterio";
+UPDATE musician SET bandName = "Crazy Duo" WHERE musicianName = "Rainbow";
+UPDATE musician SET bandName = "Luna" WHERE musicianName = "Luna";
+
